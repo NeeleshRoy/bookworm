ce5f96da74e421d73d312c6537f18baa933da4cd
Neelesh Roy <neeleshroy.2013@gmail.com>
2018-05-31 10:44:24 +0530

    Header

--- client/src/Routes/index.js
+++ client/src/Routes/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Layout from '../hoc/Layout';
+import Home from '../components/home';
 
 const Routes = () => {
-	return <div>Hello</div>;
+	return (
+		<Layout>
+			<Switch>
+				<Route path="/" exact component={Home} />
+			</Switch>
+		</Layout>
+	);
 };
 
 export default Routes;
